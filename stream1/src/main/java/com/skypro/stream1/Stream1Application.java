2efde44fd8887a10a901609a3be50c3698ef5fa2
Pavel <pavelpulle1@gmail.co>
2024-08-12 21:17:04 +0300

    initial commit

---
+++ stream1/src/main/java/com/skypro/stream1/Stream1Application.java
@@ -0,0 +1,13 @@
+package com.skypro.stream1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Stream1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Stream1Application.class, args);
+	}
+
+}
---
+++ stream1/src/test/java/com/skypro/stream1/Stream1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.stream1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Stream1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
